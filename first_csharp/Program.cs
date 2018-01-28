f7da93f8c48cb403a0ed5c1c21c5e9e8ee7e9c44
WallofYawn <indiekills@gmail.com>
2018-01-28 12:20:45 -0800

    first_csharp project uploaded

---
+++ first_csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace first_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int favoriteNum = 42;
+            // Console.WriteLine(favoriteNum);
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine("The {0} cow jumped over the {1} {2} times!", "brown", "Moon", 7);
+            // string name = "Chris";
+            // Console.WriteLine(10 + " chickens attacked " + name);
+            // string interpol = $"The answer to 2 + 7 is {2+7}";
+            // Console.WriteLine(interpol);
+        }
+    }
+}
