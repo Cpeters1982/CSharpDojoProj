0cd7fbd4884421c41746dbc71f60e16dcaf9cab6
CPeters1982 <indiekills@gmail.com>
2018-02-27 17:41:25 -0800

    Added some practice to first_csharp

--- csharp_test/Program.cs
+++ csharp_test/Program.cs
@@ -6,7 +6,58 @@ namespace csharp_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
+            
+            // int rings = 5;
+            // if (rings >= 5)
+            // {
+            //     Console.WriteLine("Welcome to the party!");
+            // }
+            // else if (rings > 2)
+            // {
+            //     Console.WriteLine("Decent... but {0} rings aren't enough", rings);
+            // }
+            // else
+            // {
+            //     Console.WriteLine(value: "Go win some more rings");
+            // }
+            // const string name = "Kobe";
+            // if (rings >= 5 && name == "Kobe")
+            // {
+            //     Console.WriteLine("Welcome to the party {0}, congratulations on your {1} rings!", name, rings);
+            // }
+            // int numOfAllStarAppearances = 17;
+            // if (rings >= 5 || numOfAllStarAppearances > 3)
+            // {
+            //     Console.WriteLine("Welcome, you are truly a legend!");
+            // }
+            // bool crazy = true;
+            // if (!crazy)
+            // {
+            //     Console.WriteLine("Let's Party!");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Sorry, you're not allowed in.");
+            // }
+            // -------------------------------------------------------------- //
+            // // Two different ways to do loops
+            // for (int i = 1; i <= 5; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            // 
+            // ------------------------------------------------------------- //
+            // For and while loops in C#
+            // for (int i = 1; i > 6; i = i + 1)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            // 
+            // ------------------------------------------------------------- //
+            // Random in C# is provided by the System namespace
+            Random rand = new Random();
+            
         }
     }
 }
