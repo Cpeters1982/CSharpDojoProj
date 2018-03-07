fb1c8900a752412950beaa998dbfef7a23717d8b
CPeters1982 <indiekills@gmail.com>
2018-03-07 11:50:50 -0800

    added some more examples to csharp_fun

--- csharp_fun/Program.cs
+++ csharp_fun/Program.cs
@@ -14,9 +14,21 @@ namespace csharp_fun
         bikes.Add("Moto Guzzi");
         bikes.Add("Harley-Davidson");
         bikes.Add("Suzuki");
-        // Accessing a generic list value is the same as you would an array
-        System.Console.WriteLine(bikes[2]); // prints "BMW"
-        System.Console.WriteLine("We currently know of {0} motorcycle manufacturers.", bikes.Count);
+        // // Accessing a generic list value is the same as you would an array
+        // System.Console.WriteLine(bikes[2]); // prints "BMW"
+        // System.Console.WriteLine("We currently know of {0} motorcycle manufacturers.", bikes.Count);
+        System.Console.WriteLine("The current manufacturers we have seen are:");
+        for (var idx = 0; idx < bikes.Count; idx++){
+            System.Console.WriteLine("-" + bikes[idx]);
+        }
+        bikes.Insert(2, "Yamaha");
+        bikes.Remove("Suzuki");
+        bikes.Remove("Yamaha");
+        bikes.RemoveAt(0);
+        System.Console.WriteLine("List of Non-Japanese Manufacturers:");
+        foreach (string manu in bikes){
+            System.Console.WriteLine("-" + manu);
+        }
         }
     }
 }
