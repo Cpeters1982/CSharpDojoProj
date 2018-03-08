d242731260c6350e9a29989d2d6333bfa6de76de
CPeters1982 <indiekills@gmail.com>
2018-03-07 16:40:41 -0800

    added csharp_dict, coded some examples in it.

---
+++ csharp_dict/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace csharp_dict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dictionary<string,string> profile = new Dictionary<string,string>();
+            // profile.Add("Name", "Speros");
+            // profile.Add("Language", "PHP");
+            // profile.Add("Location", "Greece");
+            // // System.Console.WriteLine("Instructor Profile");
+            // // System.Console.WriteLine("Name - " + profile["Name"]);
+            // // System.Console.WriteLine("From - " + profile["Location"]);
+            // // System.Console.WriteLine("Favorite Language - " + profile["Language"]);
+            // foreach (KeyValuePair<string,string> entry in profile){
+            //     System.Console.WriteLine(entry.Key + " - " + entry.Value);
+            // }
+            // // We can also use type inference, but only when the variable type  can be easily deduced... In other words, sparingly.
+            // // an example of the above code using type inference
+            // foreach (var entry in profile){
+            // System.Console.WriteLine(entry.Key + " - " + entry.Value);
+            // }
+            // int[] thisArray = new int[10];
+            // for (int i = 0; 1 <= 10; i++){
+            //     thisArray[i] = i;
+            //     System.Console.WriteLine(i);
+            // }
+            // string[] nameArray = new string[4];
+            // nameArray[0] = "Tim";
+            // nameArray[1] = "Martin";
+            // nameArray[2] = "Nikki";
+            // nameArray[3] = "Sara";
+            // foreach (string name in nameArray){
+            //     System.Console.WriteLine(name);
+            // }
+             
+        }
+    }
+}
