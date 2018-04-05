9989da17cca002c20cbb25db09fd83a48ba68554
CPeters1982 <indiekills@gmail.com>
2018-04-05 09:59:03 -0700

    Added some new examples and work

--- csharp_dict/Program.cs
+++ csharp_dict/Program.cs
@@ -18,7 +18,7 @@ namespace csharp_dict
             // foreach (KeyValuePair<string,string> entry in profile){
             //     System.Console.WriteLine(entry.Key + " - " + entry.Value);
             // }
-            // // We can also use type inference, but only when the variable type  can be easily deduced... In other words, sparingly.
+            // // We can also use type inference, but only when the variable type can be easily deduced... In other words, sparingly.
             // // an example of the above code using type inference
             // foreach (var entry in profile){
             // System.Console.WriteLine(entry.Key + " - " + entry.Value);
@@ -35,8 +35,32 @@ namespace csharp_dict
             // nameArray[3] = "Sara";
             // foreach (string name in nameArray){
             //     System.Console.WriteLine(name);
+            List<string> bikes = new List<string>();
+            bikes.Add("Kawasaki");
+            bikes.Add("Triumph");
+            bikes.Add("BMW");
+            bikes.Add("Moto Guzzi");
+            bikes.Add("harley Davidson");
+            bikes.Add("Suzuki");
+            // System.Console.WriteLine(bikes[2]);
+            // System.Console.WriteLine("We currently know of {0} motorcycle manufacturers.", bikes.Count);
+            System.Console.WriteLine("The current manufacturers we have seen are:");
+            // for(var idx = 0; idx < bikes.Count; idx++){
+            //     System.Console.WriteLine("-" + bikes[idx]);
             // }
-             
+            bikes.Insert(2, "Yamaha");
+            // for(var idx = 0; idx < bikes.Count; idx++){
+            //     System.Console.WriteLine("-" + bikes[idx]);
+            // }
+            bikes.Remove("Suzuki");
+            bikes.Remove("Yamaha");
+            bikes.RemoveAt(0);
+            System.Console.WriteLine("List of non-Japanese manufacturers:");
+            foreach (string manu in bikes){
+                System.Console.WriteLine("-" + manu);
+            }
+
         }
     }
 }
+
