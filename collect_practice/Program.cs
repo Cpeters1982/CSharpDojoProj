52382bc5fcf40291acefae985daa32f167aa0b04
CPeters1982 <indiekills@gmail.com>
2018-05-08 12:22:52 -0700

    updated collect_practice

--- collect_practice/Program.cs
+++ collect_practice/Program.cs
@@ -56,15 +56,35 @@ namespace collect_practice
             flavors.Add("Mint");
             flavors.Add("Cherry");
             flavors.Add("Bubblegum");
+            flavors.Add("Root Beer");
+            flavors.Add("Brownie");
+            flavors.Add("Vanilla");
+            flavors.Add("Banana");
             //output the length of this list after building it
             // System.Console.WriteLine(flavors.Count);
             //Output the third flavor in the list and then remove it.
             // System.Console.WriteLine(flavors[2]);
-            flavors.RemoveAt(2);
+            //flavors.RemoveAt(2);
             //output the new length of the list (it should be one less)
-            // System.Console.WriteLine(flavors.Count);
+            System.Console.WriteLine(flavors.Count);
 
-            
+            // User info dict
+            // Create a dict that will store both string keys as well as string values
+            Dictionary<string,string>  favorites = new Dictionary<string,string>();
+            //for each name in the array of names made previously, add it as a new key in this dictionary with value null
+            foreach (string name in nameArray){
+                favorites.Add(name, null);
+            }
+            //for each name  key, select a random flavor from the flavor list above and store it as the value
+            Random rand = new Random();
+            List<string> keys = new List<string>(favorites.Keys);
+            for (int i = 0; i < keys.Count; i++){
+                favorites[keys[i]] =  flavors[rand.Next(0,4)];
+            }
+            //Loop through the dict and print out each users name and favorite ice cream
+            foreach (var entry in favorites){
+                System.Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
         }
     }
 }
